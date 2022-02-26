0344ed324a5ce06a33d575a432c694fc2fb320ed
Dhruv Engineer <72173316+Dengineer24@users.noreply.github.com>
2022-02-26 13:27:31 -0500

    fix

--- src/components/infoSection/Data.js
+++ src/components/infoSection/Data.js
@@ -2,7 +2,7 @@ export const homeObjOne = {
     id: 'about', 
     lightBg: false,
     lightText: false,
-    LightTextDesc: true,
+    LightTextDesc: false,
     topLine: 'Projects',
     headline: 'I make cool stuff',
     description: 'I like Python',
@@ -17,23 +17,31 @@ export const homeObjOne = {
 
 export const homeObjTwo = {
     id: 'discover', 
+<<<<<<< Updated upstream
     lightBg: false,
+=======
+    lightBg: true,
+>>>>>>> Stashed changes
     lightText: false,
-    LightTextDesc: true,
-    topLine: 'Projects',
+    LightTextDesc: false,
+    topLine: 'Unlimited Access',
     headline: 'I make cool stuff',
     description: 'I like Python',
     buttonLabel: 'Get Started',
     imgStart: true,
     //img: require('../../images/svg-1.svg'),
     alt: 'Car',
-    dark: true,
-    primary: true,
-    darktext: false
+    dark: false,
+    primary: false,
+    darktext: true
 };
 
 export const homeObjThree = {
+<<<<<<< Updated upstream
     id: 'signup',
+=======
+    id: 'sign-up', 
+>>>>>>> Stashed changes
     lightBg: false,
     lightText: false,
     LightTextDesc: true,
