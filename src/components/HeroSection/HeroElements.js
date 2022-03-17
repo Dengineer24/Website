726bb33b4db4c71fa920edd776f75c137eafbf87
Dhruv Engineer <72173316+Dengineer24@users.noreply.github.com>
2022-03-17 16:49:05 -0400

    Update HeroElements.js

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -43,7 +43,7 @@ export const HeroContent = styled.div`
 
 export const HeroH1 = styled.h1`
     color: #fff;
-    font-size: 48px;
+    font-size: 50px;
     text-align: center;
 
     @media screen and (max-width: 768px) {
