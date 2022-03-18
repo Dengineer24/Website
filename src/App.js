c9a342eca506095764742c234ef1fcc5e657bd53
Dhruv Engineer <72173316+Dengineer24@users.noreply.github.com>
2022-03-18 11:25:46 -0400

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Home from './pages';
+import ReactGA from 'react-ga'
+import React, {useEffect} from 'react';
+
+
+
 
 function App() {
+
+  useEffect (() => {
+    ReactGA.initialize(process.env.REACT_APP_GA);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
       <Home />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ReactGA from 'react-ga'
 
-
-ReactGA.initialize(process.env.REACT_APP_GA);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
